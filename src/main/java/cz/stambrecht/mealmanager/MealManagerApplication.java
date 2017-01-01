141489ef8178e5c7d5d543eb8095d05d743b8482
Pavel Stambrecht <stambrecht.p@gmail.com>
2017-01-01 16:08:15 +0100

    Ready for war file

--- src/main/java/cz/stambrecht/mealmanager/MealManagerApplication.java
+++ src/main/java/cz/stambrecht/mealmanager/MealManagerApplication.java
@@ -2,12 +2,19 @@ package cz.stambrecht.mealmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class MealManagerApplication {
+public class MealManagerApplication extends SpringBootServletInitializer {
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(MealManagerApplication.class);
+	    }
 	
 	/**
 	 * Main method. Starts SpringApplication
